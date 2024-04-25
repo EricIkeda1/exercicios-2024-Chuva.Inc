57e6b5ceba32824f2a1b0140c044952692308c91
ikedayuji <ikedayuji.2002@gmail.com>
2024-04-25 19:42:44 -0300

    teste 14

--- ts/cypress.config.ts
+++ ts/cypress.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from "cypress";
+
 export default defineConfig({
-  env: {
-    screenshotsFolder: "./cypress/snapshots/actual",
-    trashAssetsBeforeRuns: true,
-    video: true,
-    failSilently: false,
-  },
-
-  retries: {
-    runMode: 0,
-    openMode: 0,
-  },
-
-  viewportWidth: 1280,
-  viewportHeight: 800,
-
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
   },
 });
-
