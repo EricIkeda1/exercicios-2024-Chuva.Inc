15494a4c18d259a7a5efa9d67dbb7a144ad8eecd
ikedayuji <ikedayuji.2002@gmail.com>
2024-04-25 21:01:27 -0300

    Finalizado

--- ts/cypress.config.ts
+++ ts/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "ec189971-7e2d-4dbf-93e5-594eea64dcc5",
     setupNodeEvents(on, config) {
     },
   },
